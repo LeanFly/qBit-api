475f1154e53a80ac47c42fbdb4e8a588461524ac
leanfly <leanwrt@leanwrt.local>
2021-12-03 12:00:27 +0800

    新增transmission支持

--- Dockerfile
+++ Dockerfile
@@ -12,5 +12,6 @@ EXPOSE 8000
 RUN pip install uvicorn
 RUN pip install fastapi
 RUN pip install python-qbittorrent
+RUN pip install transmission-rpc
 
 CMD [ "./qbit.py" ]
--- qbit.py
+++ qbit.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 
 from qbittorrent import Client
+from transmission_rpc import Client as trans
 from fastapi import FastAPI
 import uvicorn
+import requests
 
 app = FastAPI()
+
 
 @app.get("/qbit")
 async def qBitDownload(magnet: str):
@@ -17,5 +20,19 @@ async def qBitDownload(magnet: str):
     return "done"
 
 
+@app.get("/trans")
+async def transDownload(url: str):
+    # connect transmision
+    # host 填ip或域名，不带http；port是端口，默认是9091，有转发请填转发后的
+    tr = trans(host="127.0.0.1", port=9091, username="", password="")
+    # download；这里做了判断，如果是 .torrent 的下载链接用requests先做处理
+    key = url.split(".")[-1]
+    if key == "torrent":
+        torentUrl = requests.get(url).content
+        tr.add_torrent(torentUrl)
+    else:
+        tr.add_torrent(url)
+    return "done"
+
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run(app="qbit:app", host="0.0.0.0", port=8000, reload=True)
